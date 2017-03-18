0d0c7764ed93abfa1bfaa48aa3211e8e7943de1c
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2017-03-18 11:13:34 -0300

    first commit

---
+++ app/src/main/java/com/castrodev/marvelcharacters/model/Item.java
@@ -0,0 +1,42 @@
+
+package com.castrodev.marvelcharacters.model;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("net.hexar.json2pojo")
+@SuppressWarnings("unused")
+public class Item {
+
+    @SerializedName("name")
+    private String mName;
+    @SerializedName("resourceURI")
+    private String mResourceURI;
+    @SerializedName("type")
+    private String mType;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getResourceURI() {
+        return mResourceURI;
+    }
+
+    public void setResourceURI(String resourceURI) {
+        mResourceURI = resourceURI;
+    }
+
+    public String getType() {
+        return mType;
+    }
+
+    public void setType(String type) {
+        mType = type;
+    }
+
+}
